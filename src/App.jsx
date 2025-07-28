ab8f23f0120bdbe08b2ed42788b5d31a9e73a48e
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2025-07-28 20:17:38 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ export default function App() {
   return (
    
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/request-reset" element={<RequestReset />} />
         <Route path="/reset-password/:id/:token" element={<ResetPassword />} />
