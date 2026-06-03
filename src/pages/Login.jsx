6a4f564463de25886b3756f99bbb3509264319ec
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2026-06-03 09:40:53 +0530

    updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './auth.css';
+import './index.css';
 
 export default function Login() {
   const [email, setEmail] = useState('');
