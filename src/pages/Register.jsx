c8c538cb2de82d27ea5a9863486101af5b2ddc6f
Tharshini98 <70389909+Tharshini98@users.noreply.github.com>
2026-06-03 09:41:11 +0530

    updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './auth.css';
+import './index.css';
 
 function getStrength(pw) {
   let s = 0;
